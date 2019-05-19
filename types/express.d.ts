f455d9fda1ab6c823eda2c10d8f5772dd673d48b
Arunesh <arunesh90@outlook.com>
2019-05-20 01:37:02 +0200

    Add team property to Express requests

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,9 +1,11 @@
 import application from "../entities/application"
+import team from "../entities/applicationTeam"
 
 declare global {
   namespace Express {
     export interface Request {
       application?: application
+      team?       : team
     }
   }
 }
